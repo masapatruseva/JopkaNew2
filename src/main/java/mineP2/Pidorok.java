2cddcecae2542019ea9532c36ed86fd0ca8b8d78
masapatruseva <masapatruseva0830@mail.ru>
2025-05-20 00:56:46 +0300

    добавила смайл

--- src/main/java/mineP2/Pidorok.java
+++ src/main/java/mineP2/Pidorok.java
@@ -5,6 +5,6 @@ public class Pidorok {
     public static void main(String[] args) {
         System.out.println("Hello Pidr!");
         System.out.println("Bye Pidr!!!!");
-        System.out.println("i'm lying");
+        System.out.println("i'm lying;)");
     }
 }
