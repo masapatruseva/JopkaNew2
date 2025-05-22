4537bcafea6e926e1a23d287b47b4842f40b7cc3
Patruseva Maria <masapatruseva0830@mail.ru>
2025-05-22 20:38:25 +0300

    new Branch

--- src/main/java/mineP2/Pidorok.java
+++ src/main/java/mineP2/Pidorok.java
@@ -3,7 +3,7 @@ package mineP2;
 
 public class Pidorok {
     public static void main(String[] args) {
-        System.out.println("Hello Pidr!");
+        System.out.println("Hello Pidr! XYI");
         System.out.println("Bye Pidr!!!!");
         System.out.println("жопа");
         System.out.println("хуй;)");
